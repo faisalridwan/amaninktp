98feb8e6b154bdce719c058dbcf8ced79c0ef122
faisalridwan <faisalridwansiregar@gmail.com>
2026-02-08 22:28:33 +0700

    chore: enable allowedDevOrigins for local debugging

--- next.config.js
+++ next.config.js
@@ -5,6 +5,14 @@ const nextConfig = {
     trailingSlash: true,
     images: {
         unoptimized: true
+    },
+    // Allow access from local network
+    experimental: {
+        allowedDevOrigins: [
+            'localhost:3000',
+            '192.168.1.102:3000',
+            '192.168.1.102:3001'
+        ]
     }
 }
 
